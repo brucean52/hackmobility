d0d77ed18d278d21ecc545ac591ef61fe0a386cd
Bruce An <Bruce.An@marklogic.com>
2019-07-20 18:02:22 -0700

    added material and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'main', component: MainComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
