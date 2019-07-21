23907d6469c2bae86ea782214e3c6bc7924497c7
Bruce An <Bruce.An@marklogic.com>
2019-07-21 10:16:32 -0700

    user profile and login css

--- src/app/profile/user/user.component.ts
+++ src/app/profile/user/user.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.sass']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  userInfo: any;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userInfo = this.userService.getCurrentUserInfo();
   }
 
 }
