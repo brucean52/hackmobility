5140aab3ef4b01e400d14b31209aa1ca085b5e2a
Peng Zhou <peng.zhou@marklogic.com>
2019-07-20 19:46:39 -0700

    front end for login and registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegistrationComponent},
   { path: 'main', component: MainComponent}
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private router: Router) { }
+  username: string;
+  password: string;
 
+  ngOnInit() {
+  }
+
+  login(): void {
+    if (this.username == 'admin' && this.password == 'admin') {
+      this.router.navigate(["main"]);
+    } else {
+      alert("Invalid credentials");
+    }
+  }
 }
