86b357e20e707c24a6edfa84357e699ca8e1505e
Peng Zhou <peng.zhou@marklogic.com>
2019-07-21 02:04:21 -0700

    add profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,23 +5,31 @@ import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AuthGuard } from './core/auth.guard';
 import { CarShareComponent } from './carshare/carshare.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
+  {
+    path: '',
     canActivate: [AuthGuard],
     component: MainComponent
   },
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegistrationComponent},
-  { 
-    path: 'rideshare', 
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent },
+  {
+    path: 'rideshare',
     canActivate: [AuthGuard],
-    component: MainComponent},
-    { 
-      path: 'carshare', 
-      canActivate: [AuthGuard],
-      component: CarShareComponent}
+    component: MainComponent
+  },
+  {
+    path: 'carshare',
+    canActivate: [AuthGuard],
+    component: CarShareComponent
+  },
+  {
+    path: 'profile',
+    canActivate: [AuthGuard],
+    component: ProfileComponent
+  }
 ];
 
 @NgModule({
