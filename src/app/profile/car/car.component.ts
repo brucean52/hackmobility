28e3a5bcb1148e7f6a0eb64f30aedbdcb2f7088a
Peng Zhou <peng.zhou@marklogic.com>
2019-07-21 06:14:16 -0700

    connect car component to backent

--- src/app/profile/car/car.component.ts
+++ src/app/profile/car/car.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { CarService } from 'src/app/services/car.service';
+import { UserService } from 'src/app/services/user.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.sass']
+  styleUrls: ['./car.component.scss']
 })
 export class CarComponent implements OnInit {
+  ownerId: string;
+  carInfo: any;
 
-  constructor() { }
+  carForm = this.fb.group({
+    ownerId: [this.ownerId],
+    carName: [''],
+    make: [''],
+    model: ['']
+  })
+
+  constructor(
+      private carService: CarService,
+      private userService: UserService,
+      private fb: FormBuilder
+    ) { }
 
   ngOnInit() {
+    this.ownerId = this.userService.getCurrentUserInfo().id;
+    this.carService.getCarByOwner(this.ownerId).subscribe(
+      (result: any) => {
+        this.carInfo = result.carInfo;
+      }
+    )
+  }
+
+  addCar() {
+    console.log(this.ownerId);
+    this.carForm.patchValue({ownerId: this.ownerId});
+    return this.carService.addCarToOwner(this.carForm.getRawValue())
+      .subscribe((result) => {
+        console.log("addCarToOwner", result);
+      })
   }
 
 }
